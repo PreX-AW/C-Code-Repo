ab40582cca37769394da0846cc0e19c9504c5962
PreX <huwei040215@gmail.com>
2022-09-04 14:16:33 +0800

    bubble_Up_sort and arr_transpose

--- array/array/arr.c
+++ array/array/arr.c
@@ -11,29 +11,68 @@
 
 //**********************************交换两数组内容*******************************8
 //#include<stdio.h>
+//
+//#define Size 10
+//
+//void arr_transpose(int arr1[], int arr2[], int arr3[], int size);
+//
 //int main() {
-//	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int arr2[10] = { 11,12,13,14,15,16,17,18,19,20 };
-//	int arr3[10] = { 0 };
+//
+//	int arr1[Size] = { 1,2,3,4,5,6,7,8,9,0, };
+//
+//	int temp[Size] = { 0 };
+//
+//	int arr2[Size] = { 1,1,1,1,1,1,1,1,1,1, };
 //	int i = 0;
-//	for (; i < 10; i++) {
-//		arr3[i] = arr1[i];
-//		arr1[i] = arr2[i];
-//		arr2[i] = arr3[i];
-//
-//	}
-//	for (i = 0; i < 10; i++) {
+//	printf("Before:\n");
+//	for (i = 0; i < Size; i++) {
 //		printf("%d", arr1[i]);
-//		if (9 == i)printf("\n");
 //	}
-//	for (i = 0; i < 10; i++) {
+//	printf("\n");
+//	for (i = 0; i < Size; i++) {
 //		printf("%d", arr2[i]);
-//		if (9 == i)printf("\n");
 //	}
-//
-//	
+//	printf("\n");
+//	arr_transpose(arr1, temp,arr2 , Size);
+//	printf("After:\n");
+//	for (i = 0; i < Size; i++) {
+//		printf("%d", arr1[i]);
+//	}
+//	printf("\n");
+//	for (i = 0; i < Size; i++) {
+//		printf("%d", arr2[i]);
+//	}
 //	return 0;
 //}
+//void arr_transpose(int arr1[], int temp[], int arr2[], int size) {
+//	int i = 0;
+//	for (i = 0; i < size; i++) {
+//		temp[i] = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = temp[i];
+//		
+//	}
+//
+//
+//
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //*************************************创建一个整形数组，完成对数组的操作
 //实现函数init() 初始化数组为全0
 //实现print()  打印数组的每个元素
@@ -83,39 +122,37 @@
 //
 //}
 
-//******************************冒泡排序*****************************8
+//******************************冒泡排序(increase/up)*****************************8
 #include<stdio.h>
-#define Size 10
-void bobbule_sort(int arr[], int size);
+void bubble_Up_sort(int arr[], int size);
 int main() {
-	int arr[Size];
-	int count = 0;
-	while(0!=scanf("%d",&arr[count])){
-		count++;
-		if (count >= Size)
-			break;
-}
-	for (int i = 0; i < Size; i++) {
-		printf("%d", arr[i]);
-		if (Size - 1 == i)printf("\n");
+	int arr[10] = { 12,124,12,3,1,41,233,14,1,23 };
+	int i = 0;
+	int size = sizeof(arr) / sizeof(arr[0]);
+	printf("Before\n");
+	for (i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
 	}
-	bobbule_sort(arr, Size);
-	for (int i = 0; i < Size; i++) {
-		printf("%d", arr[i]);
+	printf("\n");
+	bubble_Up_sort(arr, size);
+	printf("After\n");
+	for (i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
 	}
 	return 0;
 }
-void bobbule_sort(int arr[], int size) {
-	int temp = 0;
-	for (int count = size-1; count <0; count--) {
-		for (int k = 0; k < count; k++) {
-			if (arr[k] > arr[k + 1]) {
-				temp = arr[k];
-				arr[k] = arr[k + 1];
-				arr[k + 1] = temp;
+void bubble_Up_sort(int arr[], int size) {
+	int count = 0;
+	int i = 0;
+	for (count=0; count < size - 1; count++) {
+		for (i=0; i < size - 1 - count; i++) {
+			int temp = 0;
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
 			}
 		}
-	
-	}
 
+	}
 }
