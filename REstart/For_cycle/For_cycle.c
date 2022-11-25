9f3c6728926b90318e0027741511d59f3a4a3b5d
Wei HU <huwei040215@gmail.com>
2022-11-25 15:19:07 +0800

    update

---
+++ REstart/For_cycle/For_cycle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+	//1 + (-1 / 2) + (1 / 3) + ... + (-1 / 100);
+	//float sum = 1;
+	//float charactor = -1;
+	//for (int i = 2; i < 101; i++) {
+	//	sum =sum+ (charactor * (1.0/ i));
+	//	charactor = -charactor;
+	//}
+	//printf("%f", sum);
+	return 0;
+	
+
+
+
+}
